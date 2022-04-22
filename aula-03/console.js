ffee09469fba4f8e90d4d56881400931b6044e98
Fred Davin <fredericodavin@gmail.com>
2022-04-22 16:53:53 -0300

    Adicionando exemplos relacionados a aula 03

---
+++ aula-03/console.js
@@ -0,0 +1,9 @@
+// log => registro
+console.log(123);
+
+// tratamento de erros
+console.error(new Error("Entrada invalida."));
+
+// table
+console.table([{ a: 1, b: 'Y', c: 3 }, { a: 'Z', b: 2 }, { c: 'H' }]);
+console.table([{ a: 1, b: 'Y', c: 3 }, { a: 'Z', b: 2 }, { c: 'H' }], ['b']);
