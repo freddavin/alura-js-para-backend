711b2262a430998ee469e6e1d5b91e555d7c052c
Fred Davin <fredericodavin@gmail.com>
2022-04-22 09:58:37 -0300

    Organizando em pastas e incluindo exemplos da aula 02

---
+++ aula-02/truthy-falsy.js
@@ -0,0 +1,9 @@
+console.log("" == false);
+console.log(0 == false);
+console.log("valor" == true); // nao tem esse caso. resultado 'false'
+console.log(1 == true);
+
+// null => vazio / aponta para nada - null no javascript é um OBJETO!
+let variavel = null;
+// undefined => valor não definido / variavel nao inicializada com algum valor
+let variavel2;
---
+++ aula-02/var-let-const.js
@@ -0,0 +1,31 @@
+// var
+
+// var forma = "retangulo";
+// var altura = 5;
+// var comprimento = 10;
+
+// if (forma === "retangulo") {
+//     area = comprimento * altura;
+// } else {
+//     area = comprimento * altura / 2;
+// }
+// console.log(area);
+// var area;
+
+// let
+
+let forma = "retangulo";
+let altura = 5;
+let comprimento = 10;
+let area;
+
+if (forma === "retangulo") {
+    area = comprimento * altura;
+} else {
+    area = comprimento * altura / 2;
+}
+console.log(area);
+//let area;
+
+// const
+// valor constante que nao pode ser alterada no decorrer do código
