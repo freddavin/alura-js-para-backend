b7b176f64b4f2a72a16c171c2b66f339c9e4bffc
Fred Davin <fredericodavin@gmail.com>
2022-04-24 23:39:05 -0300

    Finalizando exercicios de laços de repetição e criando aula de callback

--- fundamentos-do-js/arrays/lacos-de-repeticao.js
+++ fundamentos-do-js/arrays/lacos-de-repeticao.js
@@ -9,7 +9,7 @@ function retornaMediaFinal(nomeAluno) {
         }
     }
     return "Aluno não encontrado.";
-};
+}
 
 function retornaMediaFinal2(nomeAluno) {
     let index = alunos[0].findIndex(nome => nome === nomeAluno); // pode procurar com o método INCLUDES
@@ -18,7 +18,7 @@ function retornaMediaFinal2(nomeAluno) {
     } else {
         return "Aluno não encontrado."
     }
-};
+}
 
 function retornaMediaFinal3(nomeAluno) {
     let index = alunos[0].indexOf(nomeAluno); 
@@ -27,8 +27,38 @@ function retornaMediaFinal3(nomeAluno) {
     } else {
         return "Aluno não encontrado."
     }
-};
+}
 
 console.log(retornaMediaFinal("Laliny"));
 console.log(retornaMediaFinal2("Laliny"));
 console.log(retornaMediaFinal3("Laliny"));
+
+// foreach
+
+let notas = [10, 6, 8, 7];
+
+let soma = 0;
+
+notas.forEach(element => soma += element);
+
+let media = soma/notas.length;
+console.log(media);
+
+// matriz for
+console.log("-----------------")
+
+let notas2 = [10, 5, 8];
+let notas3 = [10, 9, 10];
+let matNotas = [notas, notas2, notas3];
+
+let somaTotal = 0;
+let elementos = 0;
+
+for (let i = 0; i < matNotas.length; i++) {
+    for (let j = 0; j < matNotas[i].length; j++) {
+        somaTotal += matNotas[i][j];
+        elementos++;
+    }
+}
+
+console.log(somaTotal / elementos);
